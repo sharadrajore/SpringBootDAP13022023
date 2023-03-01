5b25e65935cd2f11dc05e6f19712e496fda2083e
Sharad <sharad.rajore@gmail.com>
2023-03-01 17:31:41 +0530

    Coupon Service

--- product-managment/src/main/java/com/zensar/productmanagement/exception/CustomizedResponseEntityHandlerException.java
+++ product-managment/src/main/java/com/zensar/productmanagement/exception/CustomizedResponseEntityHandlerException.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice  
 public class CustomizedResponseEntityHandlerException extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(Exception.class)
